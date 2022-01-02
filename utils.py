6f0fbe6649d4d9b4a7b2893c94ea86c3d62ed388
mehrdad mohammadian <40211374+mehrdad-dev@users.noreply.github.com>
2022-01-02 09:39:54 +0330

    update utils

--- utils.py
+++ utils.py
@@ -42,29 +42,10 @@ def Recommend(cossim_table, input_img, k_recommend):
 
     original=load_img(input_img, target_size=(224, 224))
     img_name=input_img.split('/')[-1]
-    # plt.imshow(original)
-    # plt.show()
 
     closest_imgs=cossim_table[img_name].sort_values(ascending=False)[
                                                      1:k_recommend+1].index
     closest_imgs_scores=cossim_table[img_name].sort_values(ascending=False)[
                                                             1:k_recommend+1]
 
-    # for i in range(0, len(closest_imgs)):
-    #     original=ddl_images(closest_imgs[i], ddl_images)
-    #     plt.imshow(original)
-    #     plt.show()
-    #     sim_score = closest_imgs_scores[i]
-
     return closest_imgs, closest_imgs_scores
-
-
-def ddl_images(img_name, list_of_image):
-
-    return load_img( + img_name,
-                        target_size=(224, 224))
-
-
-
-def GetRandomImages(images_list, k_random=5):
-    return random.sample(images_list, k_random)
